e565803a87e8029a789cd4c530f4fb43d538b6b2
SantiagoGM19 <santigm1905@gmail.com>
2022-03-23 19:37:55 -0500

    Controlador de usuarios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hola Mundo");
+// Importación del framework de express
+const express = require('express');
+
+// Instanciando la aplicación
+const app = express();
+
+// Integrando el router con la app
+const router = require("./routers/index.router");
+app.use(router);
+
+// Puerto donde se levanta el servidor web
+// Puerto donde está escuchando la API
+const PORT = 3000;
+
+// Levantar la API que estará escuchando en el puerto 3000
+// 1. Primer parámetro: Puerto
+// 2. Segundo parámetro: Callback
+app.listen(PORT, () => {
+  console.log(`API escuchando en: http://localhost:${PORT}`)
+});
---
+++ src/routers/index.router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+// Instanciando un router
+const router = express.Router();
+
+// Importar controladores
+const usuario_controller = require("../controllers/usuarios.controller");
+
+// Definición de los endpoints - rutas
+
+const version = "/api/v1"
+
+router.get(version + "/usuarios", usuario_controller.consultarUsuarios);
+router.get(version + "/usuarios/:id", usuario_controller.consultarUsuario);
+router.post(version + "/usuarios", usuario_controller.crearUsuario);
+router.put(version + "/usuarios", usuario_controller.modificarUsuario);
+router.delete(version + "/usuarios", usuario_controller.eliminarUsuario);
+
+
+module.exports = router
